2c4bec03f8e19a7368b09c4ada481c3b55aaed5f
fraarf <ocscnarf333@hotmail.it>
2018-03-09 14:07:16 +0100

    Add files via upload

---
+++ Ing sw/src/v1/Login.java
@@ -0,0 +1,12 @@
+package v1;
+
+public class Login {
+	
+	int tipologin=0;
+
+	public int sceltalogin()
+	{
+		return tipologin;
+	}
+
+}
---
+++ Ing sw/src/v1/Main.java
@@ -0,0 +1,17 @@
+package v1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Login log= new Login();
+		
+		//MENU'
+		while()//condizione di accesso/ registrazione
+		{
+			
+		}
+			
+	}
+
+}
